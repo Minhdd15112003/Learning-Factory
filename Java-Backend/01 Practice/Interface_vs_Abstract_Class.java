6cb67b91d10441fa30abd8fb6ce2ec557ea32041
Minhdd15112003 <minhdd15112003@gmail.com>
2026-07-06 16:26:27 +0700

    vault backup: 2026-07-06 16:26:27

--- Java-Backend/01 Practice/Interface_vs_Abstract_Class.java
+++ Java-Backend/01 Practice/Interface_vs_Abstract_Class.java
@@ -6,7 +6,22 @@ interface Notify {
 
 }
 
+abstract class abstractNotify {
+    protected String recipient;
+
+    protected abstract void formatTimestamp();
+
+}
+
 public class Interface_vs_Abstract_Class {
+    public class EmailNotification {
+
+    }
+
+    public class SmsNotification {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
