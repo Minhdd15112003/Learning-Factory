7949db9bfd56079945f44deaaa44629882ba3dfb
Minhdd15112003 <minhdd15112003@gmail.com>
2026-07-06 16:41:47 +0700

    vault backup: 2026-07-06 16:41:47

--- Java-Backend/01 Practice/Interface_vs_Abstract_Class.java
+++ Java-Backend/01 Practice/Interface_vs_Abstract_Class.java
@@ -1,24 +1,45 @@
 /**
  * Notify
  */
-interface Notify {
+interface INotify {
     void send();
 
 }
 
-abstract class abstractNotify {
+abstract class ANotify {
     protected String recipient;
 
-    protected abstract void formatTimestamp();
+    public ANotify(String recipient) {
+        this.recipient = recipient;
+    }
+
+    protected String formatTimestamp() {
+        return "time";
+    }
 
 }
 
 public class Interface_vs_Abstract_Class {
-    public class EmailNotification {
+    public class EmailNotification extends ANotify implements INotify {
+        private String type;
 
+        public EmailNotification(String type, String recipient) {
+            super(recipient);
+            this.recipient = recipient;
+            this.type = type;
+        }
+
+        public void send() {
+
+            System.out.println(formatTimestamp() + " send to:");
+        }
     }
 
     public class SmsNotification {
+
+    }
+
+    public class context() {
 
     }
 
